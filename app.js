39919e5f2d3fd553bbfc3daf5a467ca4f3bbcc71
Ubaid Raza <154611273+ubaiidraza@users.noreply.github.com>
2025-02-03 17:29:58 +0500

    best solution

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
+var num = +prompt("Enter your number:");
 
- 
-//  var num=100;
-var num = + prompt ();
-if(num >= 1 && num <= 29 ){
-    console.log('riksha chalaly bhai');
+if (num >= 1 && num <= 100) {
+    if (num <= 29) console.log("Riksha chalaly bhai");
+    else if (num <= 39) console.log("Bachat ho gai");
+    else if (num <= 49) console.log("D grade bana ha");
+    else if (num <= 59) console.log("C grade bana ha");
+    else if (num <= 69) console.log("B grade bana ha");
+    else if (num <= 79) console.log("A grade bana ha");
+    else if (num <= 89) console.log("A+ grade bana ha");
+    else console.log("A+1 grade bana ha");
+} else {
+    console.log("Invalid number! Please enter a number between 1 and 100.");
 }
-else if(num >= 30 && num <= 39 ){
-    console.log('bchat ho gai');
-}
-else if(num >= 40 && num <= 49 ){
-    console.log('D grade bana ha');
-}
-else if(num >= 50 && num <= 59 ){
-    console.log('C grade bana ha');
-}
-else if(num >= 60 && num <= 69 ){
-    console.log('B grade bana ha');
-}
-else if(num >= 70 && num <= 79 ){
-    console.log('A grade bana ha');
-}
-else if(num >= 80 && num <= 89 ){
-    console.log('A+ grade bana ha');
-}
-else if(num >= 90 && num <= 100 ){
-    console.log('A+1 grade bana ha');
-}
-
-
-
